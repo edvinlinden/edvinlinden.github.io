e2d130a4c26bbe8217335777cdf8493c4f871e1f
Edvin Linden <edvin.linden@fortnox.se>
2025-02-02 00:06:43 +0100

    Upgrade Tailwind to version 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,13 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 import prefetch from "@astrojs/prefetch";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://edvinlinden.se",
-  integrations: [
-    mdx(),
-    tailwind({
-      // Example: Disable injecting a basic `base.css` import on every page.
-      // Useful if you need to define and/or import your own custom `base.css`.
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    sitemap(),
-    prefetch(),
-  ],
+  integrations: [mdx(), sitemap(), prefetch()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
